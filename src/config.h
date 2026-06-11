00864398680abb45ab82533616e08a54c59d5421
sventr <s.treese@me.com>
2026-06-11 21:14:00 +0200

    Set project version to V4.01

--- src/config.h
+++ src/config.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#pragma once
+
 #define PROJECT_NAME "DRILLVISION"
-#define PROJECT_VERSION "V4.1"
+#define PROJECT_VERSION "V4.01"
 
 #define DISPLAY_WIDTH 480
 #define DISPLAY_HEIGHT 480
 
 #define SENSOR_MODE_SIMULATION 1
+
