7061732ef34af5ea243618f64d8065788a3d5ac5
sventr <s.treese@me.com>
2026-06-14 20:22:42 +0200

    Add multi screen manager

--- src/screen_manager.cpp
+++ src/screen_manager.cpp
@@ -1,12 +1,47 @@
 #include "screen_manager.h"
+
 #include "screen_main.h"
+#include "screen_calibration.h"
+
+static ScreenId currentScreen = ScreenId::Main;
 
 void screen_manager_init()
 {
+    currentScreen = ScreenId::Main;
     screen_main_create();
 }
 
 void screen_manager_update()
 {
-    screen_main_update();
+    switch (currentScreen)
+    {
+        case ScreenId::Main:
+            screen_main_update();
+            break;
+
+        case ScreenId::Calibration:
+            screen_calibration_update();
+            break;
+    }
+}
+
+void screen_manager_show(ScreenId screen)
+{
+    currentScreen = screen;
+
+    switch (currentScreen)
+    {
+        case ScreenId::Main:
+            screen_main_create();
+            break;
+
+        case ScreenId::Calibration:
+            screen_calibration_create();
+            break;
+    }
+}
+
+ScreenId screen_manager_get_current()
+{
+    return currentScreen;
 }
--- src/screen_manager.h
+++ src/screen_manager.h
@@ -1,4 +1,13 @@
 #pragma once
 
+enum class ScreenId
+{
+    Main,
+    Calibration
+};
+
 void screen_manager_init();
 void screen_manager_update();
+
+void screen_manager_show(ScreenId screen);
+ScreenId screen_manager_get_current();
