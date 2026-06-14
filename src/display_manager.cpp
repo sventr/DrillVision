4ef06f5621ddd68f937f771eb719d4fce63cabb4
sventr <s.treese@me.com>
2026-06-14 18:11:41 +0200

    Connect screen manager to display manager

--- src/display_manager.cpp
+++ src/display_manager.cpp
@@ -4,12 +4,15 @@
 #include "config.h"
 #include "system_state.h"
 #include "lvgl_port.h"
+#include "screen_manager.h"
 
 void display_init()
 {
     Serial.println("Display Manager gestartet");
     Serial.println("Display Hardware noch nicht aktiv");
+
     lvgl_port_init();
+    screen_manager_init();
 }
 
 void display_update(float xAngle, float yAngle, SystemState state)
@@ -21,6 +24,7 @@ void display_update(float xAngle, float yAngle, SystemState state)
         xAngle,
         yAngle,
         system_state_to_text(state)
-        lvgl_port_update();
     );
+
+    lvgl_port_update();
 }
--- src/screen_main.cpp
+++ src/screen_main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
+
 #include "screen_main.h"
 #include "ui_model.h"
+#include "system_state.h"
 
 void screen_main_create()
 {
-    Serial.println("Main Screen erstellt");
+    Serial.println("=================================");
+    Serial.println(" BOHRVISION V4.1");
+    Serial.println(" Main Screen erstellt");
+    Serial.println("=================================");
 }
 
 void screen_main_update()
 {
+    Serial.println();
+
+    Serial.println("----------- UI -----------");
+
     Serial.printf(
-        "[UI] X=%.2f  Y=%.2f  BAT=%.0f%%\n",
-        g_uiData.xAngle,
-        g_uiData.yAngle,
+        "X: %.2f°\n",
+        g_uiData.xAngle
+    );
+
+    Serial.printf(
+        "Y: %.2f°\n",
+        g_uiData.yAngle
+    );
+
+    Serial.printf(
+        "BAT: %.0f%%\n",
         g_uiData.batteryPercent
     );
+
+    Serial.printf(
+        "STATUS: %s\n",
+        system_state_to_text(g_uiData.state)
+    );
+
+    Serial.println("--------------------------");
 }
